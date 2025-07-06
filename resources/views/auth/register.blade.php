a06f7e9155324fe5582f72db68b2847279b9b2ff
Cahya Apriyana <cahyaim3@gmail.com>
2025-07-06 19:09:45 +0700

    update verif register dan penambahan username ketika login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,13 +8,19 @@
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
-
+        <!-- Username -->
+        <div class="mt-4">
+            <x-input-label for="name" :value="__('Username')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+        </div>
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
+
 
         <!-- Password -->
         <div class="mt-4">
